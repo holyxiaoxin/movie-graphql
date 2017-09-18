7ee821ff6619432947e04cfac90382c6cd1c8ef7
Lim Jia Rong <jr.loves.maths@gmail.com>
2017-09-18 22:52:20 +0800

    sample graphql read

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -1,13 +1,30 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
+  description "The query root of this schema"
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
+  # # TODO: remove me
+  # field :testField, types.String do
+  #   description "An example field added by the generator"
+  #   resolve ->(obj, args, ctx) {
+  #     "Hello World!"
+  #   }
+  # end
+
+  field :movie do
+    type Types::MovieType
+    argument :id, !types.ID
+    resolve -> (obj, args, ctx) {
+      Movie.find(args[:id])
+    }
+  end
+
+  field :actor do
+    type Types::ActorType
+    argument :id, !types.ID
+    resolve -> (obj, args, ctx) {
+      Actor.find(args[:id])
     }
   end
 end
