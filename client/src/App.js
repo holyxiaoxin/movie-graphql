c983e4533394abab4a522848614b4055e7a45c82
Lim Jia Rong <jr.loves.maths@gmail.com>
2017-09-19 00:23:02 +0800

    plug react-apollo to graphql-ruby

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { gql, graphql } from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const { data, loading } = this.props;
+    if (loading) return null;
+
     return (
       <div className="App">
         <div className="App-header">
@@ -11,11 +15,21 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {JSON.stringify(data.movies)}
         </p>
       </div>
     );
   }
 }
 
-export default App;
+export default graphql(gql`
+  query App {
+    movies(year: 1981) {
+      title
+      year
+      actors {
+        name
+      }
+    }
+  }
+`)(App);;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:3000/graphql',
+  }),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
